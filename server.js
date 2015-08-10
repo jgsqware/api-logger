ce1bb953e720334071e101f691daca46a6c02b4c
Julien Garcia Gonzalez <julien.garcia@lampiris.be>
2015-08-10 12:10:43 +0200

    Openshift param

---
+++ server.js
@@ -0,0 +1,34 @@
+// server.js
+
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'    // set our port
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
+// START THE SERVER
+// =============================================================================
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Magic happen on " + server_ip_address + ", server_port " + server_port )
+});
